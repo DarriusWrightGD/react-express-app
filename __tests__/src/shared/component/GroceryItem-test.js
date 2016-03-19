4587369a472a7f02013a9c099e02909b24902864
Darrius Wright <darrius.wright@extendhealth.com>
2016-03-19 15:46:00 -0600

    Basic tests are running

---
+++ __tests__/src/shared/component/GroceryItem-test.js
@@ -0,0 +1,17 @@
+jest.dontMock('../../../../src/shared/components/GroceryItem');
+
+describe('GroceryItem module', function(){
+
+  describe('item purchased', function(){
+    it('should contain unbuy option', function(){
+      var CartItem = require('../../../../src/shared/components/GroceryItem');
+      
+    });
+  });
+
+  describe('item not purchased', function(){
+    it('should contain buy option', function(){
+
+    });
+  });
+});
