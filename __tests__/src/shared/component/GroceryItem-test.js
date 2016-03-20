8ddae779c204da625c9c1a5e07612715628bdfd9
Darrius Wright <darrius.wright@extendhealth.com>
2016-03-20 10:37:47 -0600

    creating first test

--- __tests__/src/shared/component/GroceryItem-test.js
+++ __tests__/src/shared/component/GroceryItem-test.js
@@ -1,16 +1,36 @@
-jest.dontMock('../../../../src/shared/components/GroceryItem');
+jest.unmock('../../../../src/shared/components/GroceryItem');
+import GroceryItem from '../../../../src/shared/components/GroceryItem';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
 
-describe('GroceryItem module', function(){
+describe('GroceryItem module', ()=>{
 
-  describe('item purchased', function(){
-    it('should contain unbuy option', function(){
-      var CartItem = require('../../../../src/shared/components/GroceryItem');
-      
+  describe('item purchased', ()=>{
+    let item;
+    let groceryItem;
+    beforeEach(()=>{
+      item = {
+        id: "1",
+        name: "Foobar",
+        purchased: true
+      }
+
+
+    });
+
+    it('should contain unbuy option', ()=>{
+      console.log(TestUtils);
+      let groceryItem = TestUtils.renderIntoDocument(
+        <GroceryItem item={item}/>
+      );
+      console.log(groceryItem);
+      var button = TestUtils.findRenderedDOMComponentWithClass(groceryItem,'grocery-item row');
+      expect(button.textContent).toEqual('Unbuy');
     });
   });
 
-  describe('item not purchased', function(){
-    it('should contain buy option', function(){
+  describe('item not purchased', ()=>{
+    it('should contain buy option', ()=>{
 
     });
   });
