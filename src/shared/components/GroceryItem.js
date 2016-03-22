0d56d5d504782d9ebd248f31a682636281acaf00
DarriusWrightGD <darriuswrightgd@gmail.com>
2016-03-22 16:21:56 -0600

    Removing commented code, and overriding circle dependency command

--- src/shared/components/GroceryItem.js
+++ src/shared/components/GroceryItem.js
@@ -30,33 +30,3 @@ export default class GroceryItem extends React.Component{
     );
   }
 }
-
-
-// export default function ({item, removeItem, updateItem}) {
-//   console.log('Render function please');
-//   let purchaseText = 'Buy';
-//   let purchaseStyle = '';
-//   if(item.purchased){
-//     purchaseText = 'Unbuy';
-//     purchaseStyle = 'button-primary';
-//   }
-//
-//   return (
-//     <div className='grocery-item row'>
-//       <div className='six columns'>
-//         <h4 className={item.purchased ? 'strikethrough' : ''}>{item.name}</h4>
-//       </div>
-//       <form className="three columns" onSubmit={(e)=>{
-//           e.preventDefault();
-//           item.purchased = !item.purchased;
-//           updateItem(item)
-//         }
-//       }>
-//         <button className={purchaseStyle}>{purchaseText}</button>
-//       </form>
-//       <form className="three columns" onSubmit={(e)=>{e.preventDefault();removeItem(item._id)}}>
-//         <button>&times;</button>
-//       </form>
-//     </div>
-//   );
-// }
